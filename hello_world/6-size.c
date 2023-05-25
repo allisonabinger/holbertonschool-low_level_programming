1961ac3e074db91628caee485bcf0af9789c0c7b
Allison <allisonabinger@gmail.com>
2023-05-25 00:08:34 -0700

    finished six pls work

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,12 +2,23 @@
 /**
  * main - Entry Point
  *
- * Description: print the size of various types on the computer it is compiled and run on
+ * Description: print the size of various types
  *
  * Return: Always 0 (Success
  */
 int main(void)
 {
-	printf("%d");
+	char charSize
+	int intSize;
+	long int longIntSize;
+	long long int longLongSize;
+	float floatSize;
+
+	printf("Size of char: %zu bytes\n", sizeof(charSize));
+	printf("Size of int: %zu bytes\n", sizeof(intSize));
+	printf("Size of long int: %zu bytes\n", sizeof(longIntSize));
+	printf("Size of long long int: %zu bytes\n", sizeof(longLongSize));
+	printf("Size of float: %zu bytes\n", sizeof(floatSize));
+
 	return (0);
 }
