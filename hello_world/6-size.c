78843d53f10a6fcd28410599367f1e7a1320ea17
Allison <allisonabinger@gmail.com>
2023-05-24 23:56:54 -0700

    not the actual task but testing something out

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: print the size of various types on the computer it is compiled and run on
+ *
+ * Return: Always 0 (Success
+ */
+int main(void)
+{
+	printf("%d");
+	return (0);
+}
