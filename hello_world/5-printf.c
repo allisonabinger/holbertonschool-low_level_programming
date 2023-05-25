20a74ea5641d89f7ac0a58e07f8f3b8c3ef350db
Allison <allisonabinger@gmail.com>
2023-05-24 23:50:45 -0700

    got betty happy again

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
- * main - Entry pont
+/**
+ * main - Entry point
+ *
+ * Description: 'printing line of text with printf
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 
 	return (0);
 }
