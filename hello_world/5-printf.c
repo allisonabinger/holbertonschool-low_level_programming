8c20f2e85f50e577681d64e9c10382328151b1c9
Allison <allisonabinger@gmail.com>
2023-05-24 23:40:18 -0700

    added 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Entry pont
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+
+	return (0);
+}
+
