6d6b58314109f1bdd26a50e26f682eef69d9ed57
Allison <allisonabinger@gmail.com>
2023-05-27 18:14:31 -0700

    trying 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for ( ; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
