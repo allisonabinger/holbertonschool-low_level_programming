ceeffd0b4d58adc6f17d05af2b0b9f27e8cefe37
Allison <allisonabinger@gmail.com>
2023-05-27 17:54:34 -0700

    moved around if statements

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,13 +13,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
-	printf("Last digit of %d is %d", n, lastDigit);
+	printf("Last digit of %d is %d and is", n, lastDigit);
+
 	if (lastDigit == 0)
 		printf("and is 0\n");
-
 	else if (lastDigit < 6)
-		printf("and is less than 6 and not 0\n");
+		printf("less than 6 and not 0\n");
 	else if (lastDigit < 5)
-		printf("and is greater than 5\n");
+		printf("greater than 5\n");
 	return (0);
 }
