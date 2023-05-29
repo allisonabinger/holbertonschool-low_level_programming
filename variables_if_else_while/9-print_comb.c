21abe5957db83d4ac359d16da3944080a831c0e4
Allison <allisonabinger@gmail.com>
2023-05-29 11:36:53 -0700

    added 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(48 + n);
+		if (n < 9)
+		{
+			putchar(44);
+			putchat(32);
+		}
+	}
+	return (0)
+}
