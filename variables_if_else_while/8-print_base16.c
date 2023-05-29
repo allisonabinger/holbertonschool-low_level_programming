1e6cea025eea752acd92532dd92f6f391e108a9b
Allison <allisonabinger@gmail.com>
2023-05-29 11:02:46 -0700

    added 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char h;
+
+	for (h = '0'; h <= '9'; h++)
+		putchar(h);
+	for (h = 'a'; h <= 'f'; h++)
+		putchar(h);
+}
