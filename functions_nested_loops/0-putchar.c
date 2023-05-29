995bfe921a970585dfb74f4a05d34fb5f1097707
Allison <allisonabinger@gmail.com>
2023-05-29 12:31:12 -0700

    stopping for today but pushing what I have

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#include <main.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	printf("_putchar\n");
-	return (0);
-}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,13 @@
+#include <unistd.h>
+
 /**
- * This is a header file
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int printf(char c);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
