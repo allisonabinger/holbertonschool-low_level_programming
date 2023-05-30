b68201f8e418b7352f91805b3b7a67163af38774
Allison <allisonabinger@gmail.com>
2023-05-30 13:24:25 -0700

    changed 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _isalpha() - checks for alphabetical character
+ * _isalpha - checks for alphabetical character
  * @c: lower or uppercase letter
  * Return: Always (0)
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 65 || c <= 90)
 		return (1);
-	else if (c >= 'A' || c <= 'Z')
+	else if (c >= 97 || c <= 122)
 	{
 		return (1);
 	}
