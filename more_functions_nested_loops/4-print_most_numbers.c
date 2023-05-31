e1b16146e184b11dda983d9fa55626a29efa0bf1
Allison <allisonabinger@gmail.com>
2023-05-31 10:39:18 -0700

    added 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers 0-9 except 2 and 4
+ *
+ * Return: Results of function
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 || n != 4)
+		{
+			_putchar(48 + n);
+		}
+	}
+	_putchar('\n');
+}
