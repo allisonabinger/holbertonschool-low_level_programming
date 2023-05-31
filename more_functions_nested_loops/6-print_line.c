c90a2679f4e8a8673fc893a37a26e864e37fe089
Allison <allisonabinger@gmail.com>
2023-05-31 12:25:37 -0700

    added 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times _ should print
+ * Return: output of print_line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		
+		for (i = 0; i < n; n++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
