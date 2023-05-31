9d35df8a4ba511a27f64092096046d5a9c08cf49
Allison <allisonabinger@gmail.com>
2023-05-31 11:29:02 -0700

    added 5 pls work

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0-14 ten times
+ *
+ * Return: output of more_numbers
+ */
+void more_numbers(void)
+{
+	int n, i;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i < 9)
+			{
+				_putchar(48 + (y / 10));
+			}
+
+			_putchar(48 + (y % 10);
+		}
+
+	_putchar('\n');
+	}
+}
