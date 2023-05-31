8d02f477ae2d3be1d385358dfb4b16f4d20184cc
Allison <allisonabinger@gmail.com>
2023-05-31 10:09:21 -0700

    added 3 updated main

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two values
+ * @a: first value
+ * @b: second value
+ * Return: returns result of mu
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
