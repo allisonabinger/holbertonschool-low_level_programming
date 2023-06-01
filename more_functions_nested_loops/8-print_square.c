f8188580c5735b69aba3923198802f9a220d44ce
Allison <allisonabinger@gmail.com>
2023-06-01 11:29:29 -0700

    fixed character errors

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,9 +10,9 @@ void print_square(int size)
 
 	for (y = 0; y < size; y++)
 	{
-		for (x = 0; x < size, x++)
+		for (x = 0; x < size; x++)
 		{
-			putchar(35);
+			_putchar(35);
 		}
 		_putchar('\n');
 	}
