321bd2bd192d7512a52c0e519458d1891aa27632
Allison <allisonabinger@gmail.com>
2023-06-01 12:00:08 -0700

    added space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 	{
 		if ((n % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		if ((n % 5) == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", n);
+			printf("%d ", n);
 		}
 	}
 	printf("\n");
