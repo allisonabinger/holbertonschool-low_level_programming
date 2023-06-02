5418f13ac0d07f2ba218a42f34513afdcad09054
Allison <allisonabinger@gmail.com>
2023-06-02 14:04:34 -0700

    added files and finished 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter and
+ * updates the value it points to to 98
+ * @n: value to be set
+ * Return: Always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	int p = 98;
+
+	*n = p;
+}
