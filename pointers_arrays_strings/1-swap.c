71598a11b54057ade53d0909df547e160638ccb0
Allison <allisonabinger@gmail.com>
2023-06-02 14:17:45 -0700

    added 0

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swaps the value of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Empty
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	int *a = *b;
+
+	int *b = c;
+}
