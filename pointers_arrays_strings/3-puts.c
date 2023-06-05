6bda662b1ebc41c21c84a67aa41ee54defa4f8ef
Allison <allisonabinger@gmail.com>
2023-06-05 14:23:37 -0700

    added 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+ * _puts - prints the string, followed by new line, to the stdout
+ * @str: string name
+ * Return: always 0 (success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	printf('\n');
+}
