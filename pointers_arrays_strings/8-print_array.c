a6b779c77bdc655d62e69b29af06d60bccdf7ef5
Allison <allisonabinger@gmail.com>
2023-06-06 12:31:09 -0700

    8 seems simple

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_array - prints the n number of elements of an array of integers
+ * @a: array input
+ * @n: number of elements of the array to be printed
+ * Return: n elements 
+ */
+void print_array(int *a, int n);
+{
+	for (; a[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+}
