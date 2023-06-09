0e45280881076fe946f978194928c444c85b83a5
Allison <allisonabinger@gmail.com>
2023-06-09 13:26:05 -0700

    added strncpy

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings, using up to n of src
+ * @dest: destination string
+ * @src: source string to concatenate from
+ * @n: maximum amount of bytes to use form src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
