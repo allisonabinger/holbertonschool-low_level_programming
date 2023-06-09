2e7f375c6f8b46cd19db435c6ba3eab4085431e1
Allison <allisonabinger@gmail.com>
2023-06-09 13:47:59 -0700

    added strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - copies src string into dest string
+ * @dest: destination string
+ * @src: source string to copy into dest
+ * @n: the amount of bytes to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != 0 && i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+	
+	dest[i] = '\0';
+
+	return (dest);
+}
