69ed29e0e54663ce53da0fa38c73354a3f66b96b
Allison <allisonabinger@gmail.com>
2023-06-10 11:53:09 -0700

    added capstring

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: S
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				s[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (s);
+}
