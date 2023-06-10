5e950c5912d181bc6974f6a090149fa9952a1375
Allison <allisonabinger@gmail.com>
2023-06-10 11:23:21 -0700

    pls

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string to input
+ * Return: output of function
+ */
+char *string_toupper(char *s)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 123 && s[i] > 96)
+		{
+			s[i] = s[i - 22];
+		}
+	}
+	return (0);
+}
