2946ba70f837964cbac1bf39300a9cfe8ced6ae2
Allison <allisonabinger@gmail.com>
2023-06-10 19:41:11 -0700

    changed to for loop

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (src[i] != '\0' && i < n)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	return (dest);
 }
