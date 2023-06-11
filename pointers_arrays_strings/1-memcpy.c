86a92c2e1ce8b454ffa44edea2567e7397aac62e
Allison <allisonabinger@gmail.com>
2023-06-10 19:37:48 -0700

    fixed memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
-		i++
+		i++;
 	}
 	return (dest);
 }
