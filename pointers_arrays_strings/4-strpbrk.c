28419fea708f3c27c5bf313779c9060907b8d5f5
Allison <allisonabinger@gmail.com>
2023-06-12 16:32:30 -0700

    d

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * +strpbrk - locates first occurrence in string s of any bytes in accept
+ * @s: pointer to string s
+ * @accept: pointer to string accept
+ * Return: pointer to byte in S or Null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, k;
+
+	int k = 0;
+
+	for (i = 0, s[i] != '\0'; i++)
+	{
+		for(j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] != accept[j])
+			{
+				s++;
+				k++
+			}
+		}
+	}
+	if (k != 0)
+	{
+		return (s);
+	}
+	return (NULL);
+}
