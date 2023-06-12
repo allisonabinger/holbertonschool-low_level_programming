d334d29d1f38344d967950cfed5261db23083cec
Allison <allisonabinger@gmail.com>
2023-06-12 15:28:26 -0700

    changed 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int a;
+
+	while (1)
 	{
-		if (*s == c)
+		a = *s++
+		if (a == c)
 		{
-			return (s);
+			return (s - 1);
 		}
+		if (a == 0)
+		{
 		return (NULL);
+		}
 	}
 }
