df1b503dd3f1a039b56fb9a4c2274bc091e3609c
Allison <allisonabinger@gmail.com>
2023-06-13 12:29:39 -0700

    added chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: 2D array to print
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; j < 8; j++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
