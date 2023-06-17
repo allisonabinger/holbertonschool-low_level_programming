3c3a3b0e4d1246c516114f3d61cd08a1c467d007
Allison <allisonabinger@gmail.com>
2023-06-17 14:44:23 -0700

    fixed main.h

--- recursion/main.h
+++ recursion/main.h
@@ -48,7 +48,7 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void _puts_recursion(char *s);
-void _print_rev_recusion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
