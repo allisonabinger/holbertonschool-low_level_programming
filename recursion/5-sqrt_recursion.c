a8da72f79d1cb676cb410d9440916d2905d43274
Allison <allisonabinger@gmail.com>
2023-06-21 11:08:16 -0700

    changed doc style

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,27 @@
 #include "main.h"
+int _result_sqrt_rec(int n, int i);
+
 /**
  * _sqrt_recursion - guesses the natural square root of a number
  * @n: number to find the square root of
  * Return: square root of @n or -1 if no natural sqrt is found
  */
-int _result_sqrt_rec(int n, int i);
+int _sqrt_recursion(int n)
+{
+        if (n < 0)
+        {
+                return (-1);
+        }
+        return (_result_sqrt_rec(n, 0));
+}
+
 /**
  * _result_sqrt_rec - returns the natural square root of a number
  * @n: number to calculate the square root of
  * @i: increment
  * Return: resulting square root
  */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	return (_result_sqrt_rec(n, 0));
-}
+
 int _result_sqrt_rec(int n, int i)
 {
 	if (i * i > n)
