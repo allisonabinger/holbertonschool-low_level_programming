786e589a32a53f27c327325f7758c35b3ad758bc
Allison <allisonabinger@gmail.com>
2023-06-22 07:43:05 -0700

    added include guards

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifdef MAIN_H
+#define MAIN_H
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -54,3 +57,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+
+#endif
