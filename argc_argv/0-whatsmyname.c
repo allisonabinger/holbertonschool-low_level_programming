3b008e83a48938dcd15ce8f67f6661ed937e62ea
Allison <allisonabinger@gmail.com>
2023-06-26 10:36:18 -0700

    fixed 0 hopefully

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-void main(char *argv[])
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%d\n", argv[0]);
+
+	return (0);
 }
