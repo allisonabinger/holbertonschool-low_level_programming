351c2a3f6c17d8dcfbe886a1d870cf5fd784a13f
Allison <allisonabinger@gmail.com>
2023-06-29 11:43:52 -0700

    chagned dog.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - structure for dog
- * @name: name of the dog, string
- * @age: age of the dog, float
- * @owner: name of the owner, string
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
 struct dog
 {
@@ -13,10 +16,16 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct d *d, typedef struct dog dog_t);
-int _putchar(char c);
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
