5ff2cf278ccb591148cbfba107f55789bcd7b20d
Allison <allisonabinger@gmail.com>
2023-06-29 11:22:51 -0700

    added dog, gitamp, putchar, readme

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is a readme that i dont want to write in quite yet
---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - structure for dog
+ * @name: name of the dog, string
+ * @age: age of the dog, float
+ * @owner: name of the owner, string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
