1561a6fc1200e91771fc59a322e4c00cf970b326
Allison <allisonabinger@gmail.com>
2023-06-29 11:42:18 -0700

    fixed dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,4 +12,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct d *d, typedef struct dog dog_t);
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
