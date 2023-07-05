d41c3e60616f065c20d92c71a5864f3875053b71
Allison <allisonabinger@gmail.com>
2023-07-05 09:44:10 -0700

    fixed betty

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from, follow by /NL
+ * Return: always 0
+ */
 int main(void)
 {
 	char *filename = __FILE__;
+
 	printf("%s\n", filename);
 	return (0);
 }
