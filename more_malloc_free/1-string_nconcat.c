56de440ca665e9e58a51d9719c2f6ea7f4706a3b
Allison <allisonabinger@gmail.com>
2023-07-05 17:46:38 -0700

    fixed variable description

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * string_nconcat: concatenates two strings
- * s1: first string
- * s2: second string
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  * n: max number of bytes of s2 to concat into s1
  * Return: NULL if function fails, or pointer to new allocated space in memory
  */
@@ -12,16 +12,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
 	unsigned int len = n, index;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
-	
 	for (index = 0; s1[index]; index++)
 		len++;
-
 	concat = malloc(sizeof(char) * (len + 1));
 
 	if (concat == NULL)
@@ -31,11 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (index = 0; s1[index]; index++)
 		concat[len++] = s1[index];
-	
 	for (index = 0; s2[index] && index < n; index++)
 		concat[len++] = s2[index];
 
 	concat[len] = '\0';
-	
 	return (concat);
 }
