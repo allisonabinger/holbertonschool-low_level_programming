0844ffa0e8896677331dabed074e1574e8019aa6
Allison <allisonabinger@gmail.com>
2023-07-05 18:51:06 -0700

    added free doggo

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,3 +1,14 @@
 #include "dog.h"
 #include <stdlib>
-
+/**
+ * free_dog - frees dog
+ * @d: doggo to freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
