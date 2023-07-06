cbc32d47ce7cbd073786e5b18ec6ee334e39500b
Allison <allisonabinger@gmail.com>
2023-07-05 18:46:29 -0700

    added new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,44 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+/**
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
 /**
  * new_dog - Creates a new dog.
  * @name: The name of the dog.
