17c2e5c54eb3c8c1213aa157c45885069f750fa5
Allison <allisonabinger@gmail.com>
2023-07-17 14:22:19 -0700

    fixed uint

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-void print_binary(unsigned long int n)
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: char string
+ * Return: success always (0)
+ */
+unsigned int binary_to_uint(const char *b)
 {
-	while (n > 0)
-	{
-		printf("%d", n & 1);
-		n >>=1;
+	unsigned int n = 0;
+	int i;
+
+	for (i = 0; b[i] != '\0'; i++) {
+		if (b[i] != '0' && b[i] != '1') {
+			return 0;
+		}
+
+		n = n << 1;
+		n += (b[i] - '0');
 	}
+
+	return (n);
 }
