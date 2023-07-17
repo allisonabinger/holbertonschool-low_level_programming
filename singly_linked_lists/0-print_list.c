787e9a87be36bb502c6b4f43cdf1b610fcd41e0b
Allison <allisonabinger@gmail.com>
2023-07-17 13:00:03 -0700

    fixed while statment

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,18 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t c = 0;
+	size_t s = 0;
 
-	if (!h->str)
-		printf("[0] (nil)\n");
-	else
-	{	
-		while (h != NULL)
-		{
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
 			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			c++;
-		}
+		h = h->next;
+		s++;
 	}
-	return (c);
+
+	return (s);
 }
