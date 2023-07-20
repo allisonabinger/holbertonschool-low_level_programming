455c17a3536a9ff83e935739571c918a95fd1432
Allison <allisonabinger@gmail.com>
2023-07-20 09:30:05 -0700

    added readme, header file, and task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistin_t *h)
+{
+	int c = 0;
+
+	if (h == NULL)
+		return (c);
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n, h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
---
+++ doubly_linked_lists/README
@@ -0,0 +1,1 @@
+This project will touch on doubly linked lists.
