0559e158f4d5820c0ecadb419b08d511e73a9f67
Allison <allisonabinger@gmail.com>
2023-07-24 09:27:27 -0700

    added 4, betty style

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dlistint - frees a list
+ * @head: head of the list
+ * Return: 0 (always success)
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head =->next;
+		free(temp);
+	}
+}
