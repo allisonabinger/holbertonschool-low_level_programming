14ba302040cb15379571c09f40934a71d7901ad5
Allison <allisonabinger@gmail.com>
2023-07-24 09:40:38 -0700

    fixed while loops

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -10,10 +10,15 @@ int sum_dlistint(dlistint_t *head)
 {
 	unsigned int sum = 0;
 
-	while (head != NULL)
+	if (head != NULL)
 	{
-		sum += head->n;
-		head = head->next;
+		while (head->prev != NULL)
+			head = head->prev;
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
 	}
 	return (sum);
 }
