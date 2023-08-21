5b0c0a7bf65c6bb4da216dd8afdc28dccede6274
allisonabinger <allisonabinger@gmail.com>
2023-08-21 16:56:14 -0700

    fixed betty/kernel

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -45,11 +45,11 @@ int binary_search(int *array, size_t size, int value)
 		if (array[mid] == value)
 			return (mid);
 
-		else if(array[mid] < value)
+		else if (array[mid] < value)
 			left = mid + 1;
 
 		else
-			right = mid -1;
+			right = mid - 1;
 	}
 
 		return (-1);
