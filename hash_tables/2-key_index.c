de3bb842842c3c8a8719892b0c61689cc2576586
allisonabinger <allisonabinger@gmail.com>
2023-08-24 11:07:42 -0700

    betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,14 +1,14 @@
 #include "hash_tables.h"
 /**
- * key_index - Calculates the index for a key in the hash table
- * @key: The key to calculate the index for
- * @size: The size of the hash table array
- * Return: The index at which the key should be stored
- */
+	* key_index - Calculates the index for a key in the hash table
+	* @key: The key to calculate the index for
+	* @size: The size of the hash table array
+	* Return: The index at which the key should be stored
+	*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
- unsigned long int hash_value = hash_djb2(key);
- unsigned long int index = hash_value % size;
+	unsigned long int hash_value = hash_djb2(key);
+	unsigned long int index = hash_value % size;
 
- return (index);
+	return (index);
 }
